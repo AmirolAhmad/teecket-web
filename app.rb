898b0d688a21b97efe54a7714bda29538b577a46
amirolahmad <9w2pdf@gmail.com>
2015-06-17 11:03:14 +0800

    move country airport list to helper, much clear in app.rb

--- app.rb
+++ app.rb
@@ -11,59 +11,7 @@ class App < Sinatra::Base
     @flights = []
     @errors = []
 
-    @airports = [
-      ['Australia', 'Darwin', 'DRW'],
-      ['Australia', 'Gold Coast', 'OOL'],
-      ['Australia', 'Melbourne', 'MEL'],
-      ['Australia', 'Perth', 'PER'],
-      ['Australia', 'Sydney', 'SYD'],
-      ['Bangladesh', 'Dhaka', 'DAC'],
-      ['Brunei Darussalam', 'Brunei', 'BWN'],
-      ['Cambodia', 'Phnom Penh', 'PNH'],
-      ['Cambodia', 'Siam Reap', 'REP'],
-      ['Hong Kong', 'Hong Kong', 'HKG'],
-      ['Indonesia', 'Bali', 'DPS'],
-      ['Indonesia', 'Balikpapan', 'BPN'],
-      ['Indonesia', 'Banda Aceh', 'BTJ'],
-      ['Indonesia', 'Bandung', 'BDO'],
-      ['Indonesia', 'Jakarta', 'CGK'],
-      ['Indonesia', 'Lombok', 'LOP'],
-      ['Indonesia', 'Makassar', 'UPG'],
-      ['Indonesia', 'Medan - Kualanamu', 'KNO'],
-      ['Indonesia', 'Padang', 'PDG'],
-      ['Indonesia', 'Palembang', 'PLM'],
-      ['Indonesia', 'Pekanbaru', 'PKU'],
-      ['Indonesia', 'Semarang', 'SRG'],
-      ['Indonesia', 'Solo', 'SOC'],
-      ['Indonesia', 'Surabaya', 'SUB'],
-      ['Indonesia', 'Yogyakarta', 'JOG'],
-      ['Indonesia', 'Pontianak', 'PNK'],
-      ['Japan', 'Osaka - Kansai', 'KIX'],
-      ['Japan', 'Sapporo - Shin-Chitose', 'CTS'],
-      ['Japan', 'Tokyo - Haneda', 'HND'],
-      ['Japan', 'Tokyo - Nareta', 'NRT'],
-      ['Laos', 'Vientiane', 'VTE'],
-      ['Macau', 'Macau', 'MFM'],
-      ['Malaysia', 'Alor Setar', 'AOR'],
-      ['Malaysia', 'Bintulu', 'BTU'],
-      ['Malaysia', 'Ipoh', 'IPH'],
-      ['Malaysia', 'Johor Bahru', 'JHB'],
-      ['Malaysia', 'Kerteh', 'KTE'],
-      ['Malaysia', 'Kota Bharu', 'KBR'],
-      ['Malaysia', 'Kota Kinabalu', 'BKI'],
-      ['Malaysia', 'Kuala Lumpur', 'KUL'],
-      ['Malaysia', 'Kuala Terengganu', 'TGG'],
-      ['Malaysia', 'Kuantan', 'KUA'],
-      ['Malaysia', 'Kuching', 'KCH'],
-      ['Malaysia', 'Labuan', 'LBU'],
-      ['Malaysia', 'Langkawi', 'LGK'],
-      ['Malaysia', 'Miri', 'MYY'],
-      ['Malaysia', 'Penang', 'PEN'],
-      ['Malaysia', 'Sandakan', 'SDK'],
-      ['Malaysia', 'Sibu', 'SBW'],
-      ['Malaysia', 'Subang', 'SZB'],
-      ['Malaysia', 'Tawau', 'TWU'],
-    ]
+    country_airport
   end
 
   get '/' do
@@ -105,3 +53,6 @@ class App < Sinatra::Base
 
   run! if app_file == $0
 end
+
+# must after class method
+require_relative 'helpers/init'
